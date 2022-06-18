74b16c1a4122936c7f0aca66ea8346322e91aa8c
Fumiya Sakai <just1factory@gmail.com>
2022-06-18 15:33:07 +0900

    第3章: iOS15以降のUINavigationBarAppearanceの調整対応

--- 03_LikeTinder/LikeTinderExample/LikeTinderExample/AppDelegate.swift
+++ 03_LikeTinder/LikeTinderExample/LikeTinderExample/AppDelegate.swift
@@ -19,38 +19,51 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
 
         // MEMO: DarkModeのキャンセル
         if #available(iOS 13.0, *) {
             window?.overrideUserInterfaceStyle = .light
         }
 
+        // MEMO: iOS15に関する配色に関する調整対応
+        setupNavigationBarAppearance()
+
         return true
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    func applicationWillResignActive(_ application: UIApplication) {}
+
+    func applicationDidEnterBackground(_ application: UIApplication) {}
+
+    func applicationWillEnterForeground(_ application: UIApplication) {}
+
+    func applicationDidBecomeActive(_ application: UIApplication) {}
+
+    func applicationWillTerminate(_ application: UIApplication) {}
+
+    // MARK: - Private Function
+
+    private func setupNavigationBarAppearance() {
+
+        // iOS15以降ではUINavigationBarの配色指定方法が変化する点に注意する
+        // https://shtnkgm.com/blog/2021-08-18-ios15.html
+
+        if #available(iOS 15.0, *) {
+            let navigationBarAppearance = UINavigationBarAppearance()
+            navigationBarAppearance.configureWithOpaqueBackground()
+
+            // MEMO: UINavigationBar内部におけるタイトル文字の装飾設定
+            navigationBarAppearance.titleTextAttributes = [
+                NSAttributedString.Key.font : UIFont(name: "HiraKakuProN-W6", size: 14.0)!,
+                NSAttributedString.Key.foregroundColor : UIColor.white
+            ]
+
+            // MEMO: UINavigationBar背景色の装飾設定
+            navigationBarAppearance.backgroundColor = UIColor(code: "#76b6e2")
+
+            UINavigationBar.appearance().standardAppearance = navigationBarAppearance
+            UINavigationBar.appearance().scrollEdgeAppearance = navigationBarAppearance
+        }
     }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
 }
 
